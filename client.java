8fa25297de3eb9b5ebc1b88b2b412ed242dc38bf
Evan O'Connor <eco2116@columbia.edu>
2016-02-29 16:39:50 -0500

    Finished initial client error handling

--- client.java
+++ client.java
@@ -20,118 +20,15 @@ public class client {
     private static final int NUM_ARGS = 6;
 
     public static void main(String[] args) {
-
-        Socket connection = setUpInteraction(args);
-
-        // Create object streams for sending/reading Message objects
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(connection.getOutputStream());
-        ObjectInputStream objectInputStream = new ObjectInputStream(connection.getInputStream());
-
-        // Read in user commands until stop is requested
-        Scanner input = new Scanner(System.in);
-        while(true) {
-            System.out.print("> ");
-            String userCmd = input.nextLine();
-            String[] splitCmd = userCmd.split(" ");
-            for(String s : splitCmd) {
-                System.out.println("cmd" + s);
-            }
-
-            if(splitCmd.length == 0) {
-                System.out.println("Please enter a command.");
-            } else {
-
-                // Stop
-                if(splitCmd[0].equals("stop")) {
-                    if(splitCmd.length == 1) {
-                        objectOutputStream.writeObject(new StopMessage());
-                        System.out.println("Goodbye!");
-                        break;
-                    } else {
-                        System.out.println("\"stop\" does not accept parameters. Try again.");
-                        continue;
-                    }
-
-                } else if(splitCmd[0].equals("get") || splitCmd[0].equals("put")) {
-                    if(splitCmd.length < 3) {
-                        System.out.println("Missing parameters. Minimum of filename and \"E\" or \"N\" required.");
-                        continue;
-
-                    // with encryption
-                    } else if(splitCmd[2].equals("E")) {
-                        if (splitCmd.length > 4) {
-                            System.out.println("Too many parameters for request with encryption.");
-                            System.out.println("Only filename, \"E\" or \"N\", and password required.");
-                            continue;
-                        } else if (splitCmd.length < 4) {
-                            System.out.println("Missing parameters for request with encryption.");
-                            System.out.println("Filename, \"E\" or \"N\", and password required.");
-                            continue;
-                        } else {
-                            System.out.println("pwd bytes : " + splitCmd[3].getBytes().length);
-                            if (splitCmd[3].getBytes().length != PASSWORD_LENGTH) {
-                                System.out.println("Password must be 8 bytes long.");
-                                continue;
-                            } else if (splitCmd[0].equals("get")) {
-                                // Get with encryption
-                                try {
-                                    handleGetEncrypted(objectOutputStream, objectInputStream, splitCmd);
-                                } catch(Crypto.RetrievalException e) {
-                                    // TODO: send error?
-                                    System.out.println(e.getMessage());
-                                } catch(Crypto.ConnectionException e) {
-                                    System.out.println(e.getMessage());
-                                }
-                            } else {
-                                // Put with encryption
-                                try {
-                                    handlePutEncrypted(objectOutputStream, splitCmd);
-                                } catch(Crypto.SendException e) {
-                                    System.out.println(e.getMessage());
-                                }
-
-                            }
-                        }
-                    // without encryption
-                    } else if(splitCmd[2].equals("N")) {
-                        if(splitCmd.length > 3) {
-                            System.out.println("Too many parameters for request without encryption.");
-                            System.out.println("Only filename and \"E\" or \"N\".");
-                            continue;
-                        } else {
-                            // Get without encryption
-                            if(splitCmd[0].equals("get")) {
-                                try {
-                                    handleGetUnencrypted(objectOutputStream, objectInputStream, splitCmd);
-                                } catch(Crypto.RetrievalException e) {
-                                    System.out.println(e.getMessage());
-                                } catch(Crypto.ConnectionException e) {
-                                    System.out.println(e.getMessage());
-                                }
-
-                            // Put without encryption
-                            } else {
-                                try {
-                                    handlePutUnencrypted(objectOutputStream, splitCmd);
-                                } catch(Crypto.ConnectionException e) {
-                                    System.out.println(e.getMessage());
-                                } catch(Crypto.SendException e) {
-                                    System.out.println(e.getMessage());
-                                }
-                            }
-                        }
-                    } else {
-                        System.out.println("Second parameter for get request must be \"E\" or \"N\"");
-                        continue;
-                    }
-                } else {
-                    System.out.println("Invalid command: " + splitCmd[0] + ". Expected \"stop\", \"put\", or \"get\".");
-                }
-            }
-
+        try {
+            Socket connection = setUpInteraction(args);
+            interactWithServer(connection);
+            connection.close();
+        } catch(IOException e) {
+            System.out.println("Failed to interact with server: Unexpected exception");
+        } catch(Crypto.ConnectionException e) {
+            System.out.println(e.getMessage());
         }
-
-        connection.close();
     }
 
     private static Socket setUpInteraction(String[] args) throws Crypto.ConnectionException {
@@ -268,5 +165,126 @@ public class client {
             throw new Crypto.SendException();
         }
 
+    }
+
+    private static void handleEncryptedCommand(String[] splitCmd, ObjectInputStream objectInputStream,
+                                               ObjectOutputStream objectOutputStream) {
+        if (splitCmd.length > 4) {
+            System.out.println("Too many parameters for request with encryption.");
+            System.out.println("Only filename, \"E\" or \"N\", and password required.");
+        } else if (splitCmd.length < 4) {
+            System.out.println("Missing parameters for request with encryption.");
+            System.out.println("Filename, \"E\" or \"N\", and password required.");
+        } else {
+            System.out.println("pwd bytes : " + splitCmd[3].getBytes().length);
+            if (splitCmd[3].getBytes().length != PASSWORD_LENGTH) {
+                System.out.println("Password must be 8 bytes long.");
+            } else if (splitCmd[0].equals("get")) {
+                // Get with encryption
+                try {
+                    handleGetEncrypted(objectOutputStream, objectInputStream, splitCmd);
+                } catch(Crypto.RetrievalException e) {
+                    // TODO: send error?
+                    System.out.println(e.getMessage());
+                } catch(Crypto.ConnectionException e) {
+                    System.out.println(e.getMessage());
+                }
+            } else {
+                // Put with encryption
+                try {
+                    handlePutEncrypted(objectOutputStream, splitCmd);
+                } catch(Crypto.SendException e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+        }
+    }
+
+    private static void handleUnencryptedCommand(String[] splitCmd, ObjectInputStream objectInputStream,
+                                                 ObjectOutputStream objectOutputStream) {
+        if(splitCmd.length > 3) {
+            System.out.println("Too many parameters for request without encryption.");
+            System.out.println("Only filename and \"E\" or \"N\".");
+        } else {
+            if(splitCmd[0].equals("get")) {
+                try {
+                    // Get without encryption
+                    handleGetUnencrypted(objectOutputStream, objectInputStream, splitCmd);
+                } catch(Crypto.RetrievalException e) {
+                    System.out.println(e.getMessage());
+                } catch(Crypto.ConnectionException e) {
+                    System.out.println(e.getMessage());
+                }
+            } else {
+                try {
+                    // Put without encryption
+                    handlePutUnencrypted(objectOutputStream, splitCmd);
+                } catch(Crypto.ConnectionException e) {
+                    System.out.println(e.getMessage());
+                } catch(Crypto.SendException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+
+    private static void interactWithServer(Socket connection) throws Crypto.ConnectionException {
+        // Create object streams for sending/reading Message objects
+        ObjectOutputStream objectOutputStream = null;
+        ObjectInputStream objectInputStream = null;
+        try {
+            objectOutputStream = new ObjectOutputStream(connection.getOutputStream());
+            objectInputStream = new ObjectInputStream(connection.getInputStream());
+        } catch(IOException e) {
+            System.out.println("Failed to set up stream with server.");
+            System.exit(1);
+        }
+
+        // Read in user commands until stop is requested
+        Scanner input = new Scanner(System.in);
+        while(true) {
+            System.out.print("> ");
+            String userCmd = input.nextLine();
+            String[] splitCmd = userCmd.split(" ");
+            for(String s : splitCmd) {
+                System.out.println("cmd" + s);
+            }
+
+            if(splitCmd.length == 0) {
+                System.out.println("Please enter a command.");
+            } else {
+                // Stop
+                if(splitCmd[0].equals("stop")) {
+                    if(splitCmd.length == 1) {
+                        try {
+                            objectOutputStream.writeObject(new StopMessage());
+                        } catch(IOException e) {
+                            throw new Crypto.ConnectionException("Failed to write stop message.");
+                        }
+                        System.out.println("Goodbye!");
+                        break;
+                    } else {
+                        System.out.println("\"stop\" does not accept parameters. Try again.");
+                    }
+                } else if(splitCmd[0].equals("get") || splitCmd[0].equals("put")) {
+                    if(splitCmd.length < 3) {
+                        System.out.println("Missing parameters. Minimum of filename and \"E\" or \"N\" required.");
+
+                        // with encryption
+                    } else if(splitCmd[2].equals("E")) {
+                        handleEncryptedCommand(splitCmd, objectInputStream, objectOutputStream);
+
+                        // without encryption
+                    } else if(splitCmd[2].equals("N")) {
+                        handleUnencryptedCommand(splitCmd, objectInputStream, objectOutputStream);
+                    } else {
+                        System.out.println("Second parameter for get request must be \"E\" or \"N\"");
+                    }
+                } else {
+                    System.out.println("Invalid command: " + splitCmd[0] + ". Expected \"stop\", \"put\", or \"get\".");
+                }
+            }
+        }
     }
 }
